819c0d511ec1565e6912b74b530c9cf3de3e104a
walescko <walescko@gmail.com>
2021-12-21 20:45:47 -0300

    Tenho que encontrar o erro ainda desse exercicio

--- Iepsen/cap02/js/resp02_A.js
+++ Iepsen/cap02/js/resp02_A.js
@@ -1,5 +1,17 @@
 function CalcularPromocao(){
     var inMedicamento = document.getElementById("inMedicamento");
     var inPreco = document.getElementById("inPreco");
-    var outPromocao = document.getElementById("outPromocao")
+    var outMedicamento = document.getElementById("outMedicamento");
+    var outPromocao = document.getElementById("outPromocao");
+
+    //var preco = Number(inPreco.value);
+    var promocao = Math.floor(inPreco.value*2);
+    var outMedicamento = inMedicamento.value;
+
+    outMedicamento.textContent = "Promocao de " + outMedicamento;
+    outPromocao.textContent = "Leve 2 por apenas R$ " + promocao.toFixed(2);
+
 }
+
+var btPromocao = document.getElementById("btPromocao");
+btPromocao.addEventListener("click", CalcularPromocao);
