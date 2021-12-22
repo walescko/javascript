b277eb259c1e644a661475ac3af6a9dc7a23f886
walescko <walescko@gmail.com>
2021-12-22 07:01:07 -0300

    NaN - no Firefox não funcionou, mesmo verificando o código e estando correto

--- Iepsen/cap02/js/resp02_A.js
+++ Iepsen/cap02/js/resp02_A.js
@@ -4,12 +4,12 @@ function CalcularPromocao(){
     var outMedicamento = document.getElementById("outMedicamento");
     var outPromocao = document.getElementById("outPromocao");
 
-    //var preco = Number(inPreco.value);
-    var promocao = Math.floor(inPreco.value*2);
-    var outMedicamento = inMedicamento.value;
+    var medicamento = inMedicamento.value;
+    var preco = Number(inPreco.value);
+    var promocao = Math.floor(preco*2);
 
-    outMedicamento.textContent = "Promocao de " + outMedicamento;
-    outPromocao.textContent = "Leve 2 por apenas R$ " + promocao.toFixed(2);
+    outMedicamento.textContent = "Promoção de " + medicamento;
+    outPromocao.textContent = "Leve 2 por apenas R$ " + promocao;
 
 }
 
