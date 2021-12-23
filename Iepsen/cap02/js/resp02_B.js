fe55edf982f38e99d82cb07e5e4d61481e9580e4
walescko <walescko@gmail.com>
2021-12-23 06:38:00 -0300

    Fixed - faltou um detalhe, agora corrigido

--- Iepsen/cap02/js/resp02_B.js
+++ Iepsen/cap02/js/resp02_B.js
@@ -6,10 +6,10 @@ function CalcularValorPagar(){
     
     var preco = Number(inPrecoUso.value);
     var tempo = Number(inTempoDeUso.value);
-    var valor = preco * (Math.ceil(tempo/15));
+    var valor = Math.ceil(tempo/15)*preco;
 
     outTempoDeUso.textContent = "Tempo de uso " + tempo + " min.";
-    outValor= "Valor a ser pago R$ " + valor.toFixed(2);
+    outValor.textContent = "Valor a ser pago R$ " + valor.toFixed(2);
 
 }
 
