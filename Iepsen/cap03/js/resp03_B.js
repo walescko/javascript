6de7e6812433f7aab9b11fc484c376f2f134b5fe
walescko <walescko@gmail.com>
2021-12-26 20:57:10 -0300

    Add validação das velocidades

--- Iepsen/cap03/js/resp03_B.js
+++ Iepsen/cap03/js/resp03_B.js
@@ -6,9 +6,21 @@ function verificarVelocidade(){
 
     var velocidadePermitida = Number(inVelocidadePermitida.value);
     var velocidadeDoVeiculo = Number(inVelocidadeDoVeiculo.value);
+    
+    if (velocidadePermitida <= 0 || isNaN(velocidadePermitida)){
+        alert("Valor inválido - informe a velocidade permitida na via!");
+        inVelocidadePermitida.focus();
+        return;
+    }
+
+    if (velocidadeDoVeiculo <= 0 || isNaN(velocidadeDoVeiculo)){
+        alert("Valor inválido - informe a velocidade do Veículo!");
+        inVelocidadePermitida.focus();
+        return;
+    }
+
     var taxaVelocidade = 1 + (velocidadeDoVeiculo - velocidadePermitida)/velocidadePermitida;
 
-    
     if (taxaVelocidade <= 1.00) {
         outResposta.textContent = "Sem Multa";
     } if (taxaVelocidade > 1.00 && taxaVelocidade <= 1.20) {
