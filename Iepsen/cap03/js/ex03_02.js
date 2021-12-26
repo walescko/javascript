20d75bbf95035f79a0f1621b2e16e25c76ac7029
walescko <walescko@gmail.com>
2021-12-26 10:31:16 -0300

    Exemplo 2 Cap 03 - Falta a funcao limpar do programa

---
+++ Iepsen/cap03/js/ex03_02.js
@@ -0,0 +1,36 @@
+function calcularPeso(){
+    var inNome = document.getElementById("inNome");
+    var rbMasculino = document.getElementById("rbMasculino");
+    var rbFeminino = document.getElementById("rbFeminino");
+    var inAltura = document.getElementById("inAltura");
+    var outResposta = document.getElementById("outResposta");
+
+    var nome = inNome.value;
+    var masculino = rbMasculino.checked;
+    var feminino = rbFeminino.checked;
+    var altura = Number(inAltura.value);
+
+
+    if (nome == "" || (masculino == false && feminino == false)){
+        alert("Por favor, informe o nome e selecione o sexo.");
+        inNome.focus();
+        return;
+    }
+
+    if (altura == 0 || isNaN(altura)){
+        alert("Por favor, informe a altura corretamente.");
+        inAltura.focus();
+        return;
+    }
+
+    if (masculino){
+        var peso = 22 * Math.pow(altura, 2);
+    } else{
+        var peso = 21 * Math.pow(altura, 2);
+    }
+
+    outResposta.textContent = nome + ": seu peso ideal é " + peso.toFixed(3) + " kg.";
+}
+
+var btCalcular = document.getElementById("btCalular");
+btCalcular.addEventListener("click", calcularPeso);
