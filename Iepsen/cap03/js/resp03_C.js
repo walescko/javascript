e393e2eada376ebd2b8f0525cb7ce1c2415b7f19
walescko <walescko@gmail.com>
2021-12-27 18:58:30 -0300

    Paquimetro - Pode-se colocar um if dentro de cada if para verificar o troco

--- Iepsen/cap03/js/resp03_C.js
+++ Iepsen/cap03/js/resp03_C.js
@@ -2,16 +2,33 @@ function paquimetro(){
 
     var inValorPago = document.getElementById("inValorPago");
     var outResposta = document.getElementById("outResposta");
-
+    var outTroco = document.getElementById("outTroco");
 
     var valorPago = Number(inValorPago.value);
+    var troco;
 
     if (valorPago < 1.00) {
         outResposta.textContent = "Saldo Insuficiente, valor minimo de R$ 1,00.";
     }
 
-    // outResposta.textContent = "Saldo Insuficiente, valor " + valorPago +", minimo R$ 1.00";
-}
+    if (valorPago >= 1.00 && valorPago < 1.75){
+        troco = valorPago - 1;
+        outResposta.textContent = "Tempo: 30 minutos."
+        outTroco.textContent = "Troco R$ " + troco.toFixed(2);
+    
+    } else if (valorPago >= 1.75 && valorPago < 3.00){
+        troco = valorPago - 1.75;
+        outResposta.textContent = "Tempo: 60 minutos";
+        outTroco.textContent = "Troco R$ " + troco.toFixed(2);
+    } else if (valorPago >= 3.00) {
+        troco = valorPago - 3;
+        outResposta.textContent = "Tempo: 120 minutos - tempo máximo"
+        outTroco.textContent = "Troco R$ " + troco.toFixed(2);
+    }
+
+
+
+    }
 
 var btExibir = document.getElementById("btExibir");
 btExibir.addEventListener("click", paquimetro);
