64b9eca3a69ba52ee0bf0815576e69c6d91bc406
walescko <walescko@gmail.com>
2021-12-27 20:33:24 -0300

    If Troco em cada if

--- Iepsen/cap03/js/resp03_C.js
+++ Iepsen/cap03/js/resp03_C.js
@@ -13,22 +13,24 @@ function paquimetro(){
 
     if (valorPago >= 1.00 && valorPago < 1.75){
         troco = valorPago - 1;
-        outResposta.textContent = "Tempo: 30 minutos."
+        outResposta.textContent = "Tempo: 30 minutos.";
+        if (troco != 0){
         outTroco.textContent = "Troco R$ " + troco.toFixed(2);
-    
+        }
     } else if (valorPago >= 1.75 && valorPago < 3.00){
         troco = valorPago - 1.75;
         outResposta.textContent = "Tempo: 60 minutos";
-        outTroco.textContent = "Troco R$ " + troco.toFixed(2);
+        if (troco != 0){
+            outTroco.textContent = "Troco R$ " + troco.toFixed(2);
+        }
     } else if (valorPago >= 3.00) {
         troco = valorPago - 3;
-        outResposta.textContent = "Tempo: 120 minutos - tempo máximo"
-        outTroco.textContent = "Troco R$ " + troco.toFixed(2);
+        outResposta.textContent = "Tempo: 120 minutos - tempo máximo";
+        if (troco != 0){
+            outTroco.textContent = "Troco R$ " + troco.toFixed(2);
+        }
     }
-
-
-
-    }
+}
 
 var btExibir = document.getElementById("btExibir");
 btExibir.addEventListener("click", paquimetro);
