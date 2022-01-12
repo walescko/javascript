c2cb35596f61a16d343c43e08f057098ade1a707
walescko <walescko@gmail.com>
2022-01-12 19:19:27 -0300

    javascript - vertice e eixo Y adiconado, correções pequenas

--- calculator/js/equacaoII.js
+++ calculator/js/equacaoII.js
@@ -1,30 +1,74 @@
-function discrimintante(){
-    let inValorQuadratico = document.getElementById("inValorQuadratico");
-    let inValorLinear = document.getElementById("inValorLinear");
-    let inValorIndependente = document.getElementById("inValorIndependente");
-    let outResolucao = document.getElementById("outResolucao");
+let inValorQuadratico = document.getElementById("inValorQuadratico");
+let inValorLinear = document.getElementById("inValorLinear");
+let inValorIndependente = document.getElementById("inValorIndependente");
+let outEquacao = document.getElementById("outEquacao");
+let outResolucao = document.getElementById("outResolucao");
+let outVertice = document.getElementById("outVertice");
+let outEixoY = document.getElementById("outEixoY")
 
-    let valorQuadratico = Number(inValorQuadratico.value);
-    let valorLinear = Number(inValorLinear.value);
-    let valorIndependente = Number(inValorIndependente.value);
+function equacao(){
+    let a = Number(inValorQuadratico.value);
+    let b = Number(inValorLinear.value);
+    let c = Number(inValorIndependente.value);
 
-    let discrimintante = Math.pow(valorLinear,2) - 4*valorQuadratico*valorIndependente;
+    outEquacao.textContent = a +"x² + " + b + "x + " + c;  
+}
+
+function raizes(){
+
+    let a = Number(inValorQuadratico.value);
+    let b = Number(inValorLinear.value);
+    let c = Number(inValorIndependente.value);
+
+    let discrimintante = Math.pow(b,2) - 4*a*c;
+
+    equacao();
     
     if (discrimintante < 0) {
         outResolucao.textContent = "Não há raízes reais";
     } else if (discrimintante == 0){
-        let raiz = valorLinear/(2*valorQuadratico);        
+        let raiz = -b/(2*a);        
         outResolucao.textContent = "Raiz da Equação é x = " + raiz;
     } else {
-        let raiz1 = (-valorLinear + Math.sqrt(discrimintante))/(2*valorQuadratico);
-        let raiz2 = (-valorLinear - Math.sqrt(discrimintante))/(2*valorQuadratico);
+        let raiz1 = (- b + Math.sqrt(discrimintante))/(2*a);
+        let raiz2 = (- b - Math.sqrt(discrimintante))/(2*a);
         outResolucao.textContent = "As raízes da equação são X1 = " + raiz1 + " e X2 = " + raiz2;
     }
 }
 
+function vertice(){
+    let a = Number(inValorQuadratico.value);
+    let b = Number(inValorLinear.value);
+    let c = Number(inValorIndependente.value);
+      
+    let vertice = -b/(2*a);
+    let y = a*Math.pow(vertice,2)+b*vertice+c;        
+    
+    equacao();
+
+    outVertice.textContent = "O vértice da Equação é X = " + vertice + " e Y = " + y;
+}
+
+function eixoY(){
+    let a = Number(inValorQuadratico.value);
+    let b = Number(inValorLinear.value);
+    let c = Number(inValorIndependente.value);
+        
+    equacao();
+
+    if (c == 0){
+        outEixoY.textContent = "A equação não cruza o eixo Y";
+    } else {
+        outEixoY.textContent = "A equação cruza o eixo Y em " + c;
+    }
+}
 
 let btRaizes = document.getElementById("btRaizes");
-btRaizes.addEventListener("click", discrimintante);
+let btVertice = document.getElementById("btVertice");
+let btEixoY = document.getElementById("btEixoY");
+btRaizes.addEventListener("click", raizes);
+btVertice.addEventListener("click", vertice);
+btEixoY.addEventListener("click", eixoY);
 
 function limparDados(){
     location.reload();
