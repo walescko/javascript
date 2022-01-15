34b8800c45a54d8767596eaae8604b6b6700a272
walescko <walescko@gmail.com>
2022-01-15 19:50:27 -0300

    Javascript - tentando resolver os pares e impares da strinh, posição

--- Iepsen/cap06/js/resp06_A.js
+++ Iepsen/cap06/js/resp06_A.js
@@ -1,8 +1,41 @@
 let mensagemCriptografada = "";
 
 function criptografar(){
-    let inCriptografar = document.getElementById("inCriptografar")
+    let inMensagem = document.getElementById("inMensagem");
+    let outCriptografia = document.getElementById("outCriptografia");
 
+
+    let mensagem = inMensagem.value;
+    let tamanho = mensagem.length;
+
+    
+    let criptoPar = [];
+    let criptoImpar = [];
+        
+    for (let i = 0 ; i <= tamanho ; i = i+2){
+        if ( i % 2 == 0 ){
+            criptoPar[i] = mensagem.charAt(i);
+        } 
+    }
+
+    for (let i = 0 ; i <= tamanho ; i = 2*i+1){
+        if ( i % 2 != 0 ){
+            criptoImpar[i] = mensagem.charAt(i);
+        } 
+    }
+
+    for (let i = 0; i < tamanho/2; i++){
+        if (i % 2 == 0){
+        mensagemCriptografada = mensagemCriptografada + criptoPar[i];
+    }
+
+
+    for (let i = 0; i < tamanho/2; i++){
+        if (i % 2 != 0){
+        mensagemCriptografada = mensagemCriptografada + criptoImpar[i];
+    }
+
+    outCriptografia.textContent = "Mensagem a criptografar: " + mensagemCriptografada;
 }
 
 function descriptografar(){
@@ -18,5 +51,5 @@ btDescriptografar.addEventListener("click", descriptografar);
 function limparDados(){
     location.reload();
 }
-let btLimpar = document.getElementById("btNovaConta");
+let btLimpar = document.getElementById("btLimpar");
 btLimpar.addEventListener("click", limparDados);
