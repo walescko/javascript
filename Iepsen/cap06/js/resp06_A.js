dc0c1cbe7a6e9b6081a730a91673c80ee3d74032
walescko <walescko@gmail.com>
2022-01-15 20:51:09 -0300

    javascript - ok para pares e impares, agora vamos a outra parte

--- Iepsen/cap06/js/resp06_A.js
+++ Iepsen/cap06/js/resp06_A.js
@@ -4,42 +4,28 @@ function criptografar(){
     let inMensagem = document.getElementById("inMensagem");
     let outCriptografia = document.getElementById("outCriptografia");
 
-
     let mensagem = inMensagem.value;
     let tamanho = mensagem.length;
 
     
-    let criptoPar = [];
-    let criptoImpar = [];
+    let criptoPar = "";
+    let criptoImpar = "";
         
-    for (let i = 0 ; i <= tamanho ; i = i+2){
-        if ( i % 2 == 0 ){
-            criptoPar[i] = mensagem.charAt(i);
-        } 
-    }
-
-    for (let i = 0 ; i <= tamanho ; i = 2*i+1){
+    for (let i = 0 ; i <= tamanho ; i++){
         if ( i % 2 != 0 ){
-            criptoImpar[i] = mensagem.charAt(i);
+            criptoImpar = criptoImpar + mensagem.charAt(i);
+        } else {
+            criptoPar = criptoPar + mensagem.charAt(i);
         } 
     }
 
-    for (let i = 0; i < tamanho/2; i++){
-        if (i % 2 == 0){
-        mensagemCriptografada = mensagemCriptografada + criptoPar[i];
-    }
-
-
-    for (let i = 0; i < tamanho/2; i++){
-        if (i % 2 != 0){
-        mensagemCriptografada = mensagemCriptografada + criptoImpar[i];
-    }
-
-    outCriptografia.textContent = "Mensagem a criptografar: " + mensagemCriptografada;
+    mensagemCriptografada = criptoPar + criptoImpar;
+    
+    outCriptografia.textContent = "Mensagem a criptografada: " + criptoPar + criptoImpar;
 }
 
 function descriptografar(){
-    let inDescriptografar = document.getElementById("inDescriptografar")
+    let inDescriptografar = document.getElementById("inDescriptografar");
 
 }
 
