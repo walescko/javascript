36f273e19958bf1daf454f56e7d2115c0cfd8c98
walescko <walescko@gmail.com>
2022-01-25 06:41:52 -0300

    javascript - exemplo 7.3 - function mostrarDados()

---
+++ Iepsen/cap07/js/ex07_03.js
@@ -0,0 +1,32 @@
+function mostrarDados(){
+    let inModelo = document.getElementById("inModelo");
+    let inAno = document.getElementById("inAno");
+    let inPreco = document.getElementById("inPreco");
+    let outClassificar = document.getElementById("outClassificar");
+    let outPrecoVenda = document.getElementById("outPrecoVenda");
+
+    let modelo = inModelo.value;
+    let ano = Number(inAno.value);
+    let preco = Number(inPreco.value);
+
+    if (modelo == "" || ano == 0 || preco == 0 || isNaN(ano) || isNaN(preco)){
+        alert("Informe corretamente os dados do veículo!");
+        inModelo.focus();
+        return;
+    }
+
+    let classificacao = classificarVeiculo(ano);
+    let precoVenda = calcularVenda(preco, classificacao);
+
+    outClassificar.textContent = modelo + " - " + classificacao;
+    outPrecoVenda.textContent = "Preço de Venda R$ " + precoVenda.toFixed(2);
+}
+
+let btCalucular = document.getElementById("btCalcular");
+btCalucular.addEventListener("click", mostrarDados);
+
+function limparDados(){
+    location.reload();
+}
+let btLimpar = document.getElementById("btLimpar");
+btLimpar.addEventListener("click", limparDados);
