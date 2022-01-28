62ee62bad81d7f2b9888a9a11be0782843987d65
walescko <walescko@gmail.com>
2022-01-28 07:19:19 -0300

    javascript - função trocar item

--- Iepsen/cap07/js/ex07_06.js
+++ Iepsen/cap07/js/ex07_06.js
@@ -1,3 +1,24 @@
+let itens = []
+
+function trocarItem(){
+    let inPizza = document.getElementById("inPizza");
+    let inBebida = document.getElementById("inBebida");
+
+    if (rbPizza.checked){
+        inBebida.className = "oculta";
+        inPizza.className = "exibe";
+    } else {
+        inPizza.className = "oculta";
+        inBebida.className = "exibe";
+    }
+}
+
+let rbPizza = document.getElementById("rbBebida");
+let rbBebida = document.getElementById("rbBebida");
+rbPizza.addEventListener("change", trocarItem);
+rbBebida.addEventListener("change", trocarItem);
+
+
 function limparDados(){
     location.reload();
 }
