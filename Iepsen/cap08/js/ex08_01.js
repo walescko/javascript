5e216f340cb2990f43cc767f11215d0d24f086ee
walescko <walescko@gmail.com>
2022-02-06 09:26:52 -0300

    javascript - exemple 8.1 0 cap 08

---
+++ Iepsen/cap08/js/ex08_01.js
@@ -0,0 +1,46 @@
+function trocarClube(){
+    let imgClube = document.getElementById("imgClube");
+    let divTitulo = document.getElementById("divTitulo");
+
+    let clube;
+
+    if (rbBrasil.checked){
+        clube = "Brasil";
+    }else if (rbCaxias.checked){
+        clube = "Caxias";
+    } else if (rbFarroupilha.checked){
+        clube = "Farroupilha";
+    } else if (rbGremio.checked){
+        clube = "Gremio";
+    } else if (rbJuventude.checked){
+        clube = "Juventude";
+    } else if (rbPelotas.checked){
+        clube = "Pelotas";
+    } else {
+        clube = "Sao Paulo";
+    }
+
+    divTitulo.className = "row cores" + clube;
+
+    imgClube.src = "img/" + clube.toLowerCase() + ".png";
+    imgClube.className = "exibe";
+    imgClube.alt = "Símbolo do " + clube;
+
+    localStorage.setItem("clube", clube);
+}
+
+let rbBrasil = document.getElementById("rbBrasil");
+let rbCaxias = document.getElementById("rbCaxias"); 
+let rbFarroupilha = document.getElementById("rbFarroupilha");
+let rbGremio = document.getElementById("rbGremio");
+let rbJuventude = document.getElementById("rbJuventude");
+let rbPelotas = document.getElementById("rbPelotas");
+let rbSaoPaulo = document.getElementById("rbSaoPaulo");
+
+rbBrasil.addEventListener("change", trocarClube);
+rbCaxias.addEventListener("change", trocarClube);
+rbFarroupilha.addEventListener("change",trocarClube);
+rbGremio.addEventListener("change", trocarClube);
+rbJuventude.addEventListener("change", trocarClube);
+rbPelotas.addEventListener("change", trocarClube);
+rbSaoPaulo.addEventListener("change", trocarClube);
