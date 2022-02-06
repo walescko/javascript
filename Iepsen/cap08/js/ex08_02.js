bccaded79a9ec8f6ebe56f882b2385a6836c8ff7
walescko <walescko@gmail.com>
2022-02-06 15:30:47 -0300

    javascript - add function verificar clube

--- Iepsen/cap08/js/ex08_02.js
+++ Iepsen/cap08/js/ex08_02.js
@@ -30,3 +30,28 @@ let inputsRadio = document.getElementsByTagName("input");
 for (let i = 0; i < inputsRadio.length; i++){
     inputsRadio[i].addEventListener("change", trocarClube);
 }
+
+function verificarClube(){
+
+    if (localStorage.getItem("clube")){
+        let clube = localStorage.getItem("clube");
+
+        if(clube == "Brasil"){
+            inputsRadio[0].checked = true;
+        } else if (clube == "Caxias"){
+            inputsRadio[1].checked = true;
+        } else if (clube == "Faroupilha"){
+            inputsRadio[2].checked = true;
+        } else if (clube == "Gremio"){
+            inputsRadio[3].checked = true;
+        } else if (clube == "Juventude"){
+            inputsRadio[4].checked = true;
+        } else if (clube == "Pelotas"){
+            inputsRadio[5].checked = true;
+        } else {
+            inputsRadio[6].checked = true;
+        }
+        trocarClube();
+    }
+}
+verificarClube();
