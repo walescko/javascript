d4b385fb6b48d481eb007bca45872521f894bf60
walescko <walescko@gmail.com>
2022-02-08 07:57:57 -0300

    javascript - function apostar

---
+++ Iepsen/cap08/js/ex08_03.js
@@ -0,0 +1,41 @@
+function incluirAposta(){
+    let inNome = document.getElementById("inNome");
+    let inPeso = document.getElementById("inPeso");
+
+    let nome = inNome.value;
+    let peso = Number(inPeso.value);
+
+    if(nome == "" || peso == 0 || isNaN(peso)){
+        alert("Informe nome do apostador e peso da aposta");
+        inNome.focus();
+        return;
+    }
+
+    if (varApostaExiste(peso)){
+        alert("Já realizaram essa aposta!\n Informe outro valor!")
+        inPeso.focus()
+        return;
+    }
+
+    if (localStorage.getItem("melanciaNome")){
+
+        let melanciaNome = localStorage.getItem("melanciaNome") + ";" + nome;
+        let melanciaPeso = localStorage.getItem("melanciaPEso") + ";" + peso;
+
+        localStorage.setItem("melanciaNome", melanciaNome);
+        localStorage.setItem("melanciaPeso", melanciaPeso);
+    } else {
+        localStorage.setItem("melanciaNome", nome);
+        localStorage.setItem("melanciaPeso", peso);
+    }
+
+    mostrarApostas();
+
+    inNome.value = "";
+    inPeso.value = "";
+    inNome.focus();
+}
+
+let btApostar = document.getElementById("btApostar");
+btApostar.addEventListener("click", incluirAposta);
+
