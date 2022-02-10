c5ff4ff4f3c7e63018acad46550a5a738d775e91
walescko <walescko@gmail.com>
2022-02-10 09:44:45 -0300

    javascript - add retire select function

--- Iepsen/cap09/js/ex09_01.js
+++ Iepsen/cap09/js/ex09_01.js
@@ -51,3 +51,30 @@ function selecionarTarefa(){
 
 let btSelecionar = document.getElementById("btSelecionar");
 btSelecionar.addEventListener("click", selecionarTarefa);
+
+function retirarSelecionada(){
+    let divQuadro = document.getElementById("divQuadro");
+    let h5 = document.getElementsByTagName(h5);
+    let numH5 = h5.length;
+
+    let aux = -1;
+
+    for (let i = 0; i < numH5; i++){
+        if(h5[i].className == "tarefaSelecionada"){
+            aux = i;
+            break;
+        }
+    }
+
+    if (aux == -1) {
+        alert("Selecione uma tarefa para removê-la...");
+        return;
+    }
+
+    if (confirm("Confirma Exclusão de '" +h5[aux].textContent + "'?")){
+        divQuadro.removeChild(h5[aux]);
+    }
+}
+
+let btRetirar = document.getElementById("btRetirar");
+btRetirar.addEventListener("click", retirarSelecionada);
