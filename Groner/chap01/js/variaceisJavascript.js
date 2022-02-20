47c11473054659f4fb7450ce9d3e1d79c048422b
walescko <walescko@gmail.com>
2022-02-20 16:05:23 -0300

    javascript - variables in javascript

---
+++ Groner/chap01/js/variaceisJavascript.js
@@ -0,0 +1,29 @@
+let num =1;
+num = 3; 
+let price = 1.5;
+let myName = "Japir Adão";
+let trueValue = true;
+let nullVar = null;
+let und;
+
+console.log('num: ' + num);
+console.log('myName: ' + myName);
+console.log('trueValue: ' + trueValue);
+console.log('price: ' + price);
+console.log('nullVar: ' + nullVar);
+console.log('und: ' + und);
+
+// alert('num: ' + num);
+// alert('myName: ' + myName);
+// alert('trueValue: ' + trueValue);
+// alert('price: ' + price);
+// alert('nullVar: ' + nullVar);
+// alert('und: ' + und);
+
+document.write('num: ' + num + '<br>');
+document.write('myName: ' + myName + '<br>');
+document.write('trueValue: ' + trueValue + '<br>');
+document.write('price: ' + price + '<br>');
+document.write('nullVar: ' + nullVar +  '<br>');
+document.write('und: ' + und +  '<br>');
+
