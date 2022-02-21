0dce2ab7223aa16139eb5092c461bffcb6a77df1
walescko <walescko@gmail.com>
2022-02-21 05:58:47 -0300

    javascript - operators

---
+++ Groner/chap01/js/operadores.js
@@ -0,0 +1,36 @@
+console.log('Operadores em JavaScript');
+document.write('Operadores em Java Script <br>' );
+
+let num = 0;
+num = num + 2;
+num = num * 3;
+num = num / 2;
+
+num++;
+num--;
+num += 1;
+num -= 2;
+num *= 3;
+num /= 2;
+num %= 3;
+
+console.log("num == 1: " + (num == 1));
+document.write("num == 1: " + (num == 1) + "<br>");
+console.log("num === 1: "+ (num === 1));
+document.write("num === 1: "+ (num === 1)+ "<br>");
+console.log("num != 1: " + (num != 1));
+document.write("num != 1: " + (num != 1)+ "<br>");
+console.log("num > 1: " + (num > 1));
+document.write("num > 1: " + (num > 1)+ "<br>");
+console.log("num < 1: " + (num < 1));
+document.write("num < 1: " + (num < 1) + "<br>");
+console.log("num >= 1: " + (num >= 1));
+document.write("num >= 1: " + (num >= 1) + "<br>");
+console.log("num <= 1: " + (num <= 1));
+document.write("num <= 1: " + (num <= 1) + "<br>")
+console.log("true && False: " + (true && false));
+document.write("true && False: " + (true && false) + "<br>")
+console.log("true || false: " + (true || false));
+document.write("true || false: " + (true || false) + "<br>")
+console.log("!true: " + (!true));
+document.write("!true: " + (!true) + "<br>")
