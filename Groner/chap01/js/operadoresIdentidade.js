10c7abb327f889f34b81215ff49373e26461f7d7
Walescko <walescko@gmail.com>
2022-07-10 18:23:06 -0300

    html/css/js - Operators, adjust files

---
+++ Groner/chap01/js/operadoresIdentidade.js
@@ -0,0 +1,7 @@
+console.log('Operadores de Igualdade == e ===');
+
+console.log('packt' === true);
+console.log('packt' === 'packt');
+let person1 = {name: 'Jonh'};
+let person2 ={name: 'Jonh'};
+console.log(person1===person2);
