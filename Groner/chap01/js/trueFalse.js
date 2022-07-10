fc1c11cb3531edf5359a064051b1e7811c8d3d68
Walescko <walescko@gmail.com>
2022-07-10 08:15:55 -0300

    js - true and false

---
+++ Groner/chap01/js/trueFalse.js
@@ -0,0 +1,21 @@
+function testTruthy(val){
+    return val ? console.log('truthy') : console.log('falsy');
+}
+
+testTruthy(true);
+testTruthy(false);
+testTruthy(new Boolean(false));
+testTruthy('');
+testTruthy('Packt');
+testTruthy(new String(''));
+testTruthy(1);
+testTruthy(-1);
+testTruthy(NaN);
+testTruthy(new Number(NaN));
+testTruthy({});
+
+var obj = {name: 'Jonh'};
+testTruthy(obj);
+testTruthy(obj.name);
+testTruthy(obj.age);
+
