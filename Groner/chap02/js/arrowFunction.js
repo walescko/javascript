db737f97a235250821da4055e200ed7465f5863a
Walescko <walescko@gmail.com>
2022-07-17 18:01:02 -0300

    js - arrow function

--- Groner/chap02/js/arrowFunction.js
+++ Groner/chap02/js/arrowFunction.js
@@ -1,11 +1,11 @@
-let circleAreaES5 = function circleArea(r){
+let circleArea = r =>{
     const PI = 3.14;
     let area = PI*r*r;
     return area;
 }
 
 function exitData(){
-    console.log(circleAreaES5(2));
+    console.log(circleArea(2));
 
     }
 
