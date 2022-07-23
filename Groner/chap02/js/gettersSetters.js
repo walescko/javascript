0c2e52181daf0e9e68a2ed2c0fd80dca514dd3b2
Walescko <walescko@gmail.com>
2022-07-22 22:39:33 -0300

    js - Heritage, getters and setters

---
+++ Groner/chap02/js/gettersSetters.js
@@ -0,0 +1,6 @@
+function exitData(){
+    console.log('Getters and Setters');
+}
+
+let btObject03 = document.getElementById('btObject03');
+btObject03.addEventListener('click', exitData);
--- Groner/chap02/js/heritage.js
+++ Groner/chap02/js/heritage.js
@@ -1,17 +1,18 @@
 class ITBook extends Book{
     constructor(title, pages, isbn, technology) {
         super(title, pages, isbn);
-        this.technoloy = technology;
+        this.technology = technology;
     }
     printTechnology(){
-        console.log(this.technoloy());
+        console.log(this.technology);
     }
 }
 
+let jsBook = new ITBook('Learning JS Algorithms', '200', "12345678", 'Javascript');
 function exitData() {
-    let jsBook = new ITBook('Learning JS Algorithms', '200', "12345678", 'Javascript');
     console.log(jsBook.title);
-    console.log(jsBook.printTechnology());
+    // console.log(jsBook.printTechnology());
+    jsBook.printTechnology();
 }
 
 let btObject02 = document.getElementById('btObject02');
