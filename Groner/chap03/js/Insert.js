9631242239b0966ed51e6f6a5b43a0b1ba25d458
Walescko <walescko@gmail.com>
2022-07-29 21:46:49 -0300

    javascript - insert First Position

--- Groner/chap03/js/Insert.js
+++ Groner/chap03/js/Insert.js
@@ -1,12 +1,29 @@
 let numbers = [0,1,2,3,4,5,6,7,8,9,10];
 
 function Inserts(){
-    for(let i = 0; i<numbers.length; i++) {
-        console.log(numbers[i]);
-    }
+    console.log("Antes da inserção:");
+    Numbers();
+    numbers.push(11);
+    numbers.push(12,13);
+    console.log("Após inserção:");
+    Numbers();
+
 }
 
 function InsertFirst(){
+    console.log("Antes de descolar os elementos:");
+    Numbers();
+
+    Array.prototype.insertFirstPosition = function (value){
+        for(let i = this.length; i>=0; i--){
+            this[i] = this[i-1];
+        }
+        this[0]=value;
+    };
+    numbers.insertFirstPosition(-1);
+
+    console.log("Após o deslocamento e inserção no inicio:")
+    Numbers();
 
 }
 
@@ -15,6 +32,7 @@ function Numbers(){
         console.log(numbers[i]);
     }
 }
+
 let btNumbers = document.getElementById("btNumbers");
 btNumbers.addEventListener("click", Numbers);
 let btInsertPush = document.getElementById("btInsertPush");
