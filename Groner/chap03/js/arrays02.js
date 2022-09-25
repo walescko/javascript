366cb649ab554c4e8dbbdcd5441b1add96e5e386
Walescko <walescko@gmail.com>
2022-09-25 20:43:21 -0300

    js - two dimensions

--- Groner/chap03/js/arrays02.js
+++ Groner/chap03/js/arrays02.js
@@ -1,7 +1,25 @@
+let averageTemp = [];
+averageTemp[0] = [75, 72, 74, 79, 82, 81]
+averageTemp[1] = [80, 79, 73, 75, 73, 74]
+
 function HelloArrays() {
-    ;
     alert("Hello Arrays");
+
+}
+
+function printArray2D(matrix){
+    for (let i=0; i<matrix.length; i++){
+        for(let j=0; j < matrix[i].length; j++){
+            console.log(matrix[i][j]);
+        }
+    }
+}
+
+function printMatrix(){
+    printArray2D(averageTemp);
 }
 
 let btHelloArrays = document.getElementById("btHello");
 btHelloArrays.addEventListener("click", HelloArrays);
+let btArray2D = document.getElementById("btArray2D");
+btArray2D.addEventListener("click", printMatrix);
