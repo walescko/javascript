5a08e860f52aa62f186ca3fd9c011b951da9fe36
Walescko <walescko@gmail.com>
2022-10-06 11:47:54 -0300

    html/js - map and filter

--- Groner/chap03/js/interactionArrays.js
+++ Groner/chap03/js/interactionArrays.js
@@ -3,8 +3,13 @@ let numberss = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 function isEven(x){
     console.log(x);
     return (x%2 == 0) ? true : false;
-
 }
+
+function isEven1(x){
+    // console.log(x);
+    return (x%2 == 0) ? true : false;
+}
+
 function everyFunction() {
     numberss.every(isEven);
 }
@@ -13,13 +18,31 @@ function someFunction() {
     numberss.some(isEven);
 }
 
-// function printEvens(){
-//     for(let i = 0; i < numberss.length; i++){
-//         isEven(numberss[i]);
-//     }
-// }
+function forEachFunction(){
+    numberss.forEach(x=> console.log(x%2 === 0));
+}
+
+function myMapFunction(){
+    const myMap = numberss.map(isEven1);
+    for(let i = 0; i<=myMap.length; i++){
+        console.log(myMap[i]);
+    }
+}
+
+function filterFunction(){
+    const evenNumbers = numberss.filter(isEven1);
+    for(let i = 0; i<=evenNumbers.length; i++){
+        console.log(evenNumbers[i]);
+    }
+}
 
 let btEvery = document.getElementById("btEvery");
 btEvery.addEventListener("click", everyFunction);
 let btSome = document.getElementById("btSome");
 btSome.addEventListener("click", someFunction);
+let btForEach = document.getElementById("btForEach");
+btForEach.addEventListener("click", forEachFunction);
+let btMyMap = document.getElementById("btMyMap");
+btMyMap.addEventListener("click", myMapFunction);
+let btFilter = document.getElementById("btFilter");
+btFilter.addEventListener("click", filterFunction);
