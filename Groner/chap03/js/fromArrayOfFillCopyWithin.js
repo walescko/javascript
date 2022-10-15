430decc22b2db2e25af3f21a4feccbdb68830166
Walescko <walescko@gmail.com>
2022-10-15 07:47:05 -0300

    js - fill and copyWithin methods

--- Groner/chap03/js/fromArrayOfFillCopyWithin.js
+++ Groner/chap03/js/fromArrayOfFillCopyWithin.js
@@ -18,9 +18,35 @@ function arraysOf4(){
     console.log(numbersCopy);
 }
 
+function fillMethod(){
+    let numbersCopy = Array.of(2, 3 , 5, 9 , 11, 33);
+    console.log(numbersCopy);
+    numbersCopy.fill(0);
+    console.log(numbersCopy);
+    numbersCopy.fill(2,1);
+    console.log(numbersCopy);
+    numbersCopy.fill(1,3,5)
+    console.log(numbersCopy);
+}
+
+function copyWithinMethod(){
+    let numbersArray = [1, 2, 3, 4, 5, 6];
+    console.log(numbersArray);
+    numbersArray.copyWithin(0,3);
+    console.log(numbersArray);
+    numbersArray = [1, 2, 3, 4, 5, 6];
+    numbersArray.copyWithin(1,3,5)
+    console.log(numbersArray);
+
+}
+
 let btFromEvens = document.getElementById("btFromEvens");
 btFromEvens.addEventListener("click", fromEvensMethod);
-let brFromOdds = document.getElementById("btFromOdds");
-brFromOdds.addEventListener("click", fromOddsMethod);
+let btFromOdds = document.getElementById("btFromOdds");
+btFromOdds.addEventListener("click", fromOddsMethod);
 let btArrayOf = document.getElementById("btArrayOf");
 btArrayOf.addEventListener("click", arraysOf4);
+let btFill = document.getElementById("btFill");
+btFill.addEventListener("click", fillMethod);
+let btWithin = document.getElementById("btCopyWithin");
+btWithin.addEventListener("click", copyWithinMethod);
