4128395dd24330b7470084f8826aab779c5f159c
Walescko <walescko@gmail.com>
2022-10-16 13:47:12 -0300

    html/js - toString and others methods

---
+++ Groner/chap03/js/research.js
@@ -0,0 +1,37 @@
+let numbers06 = [1, 2, 3, 4, 5, 6, 7, 8,
+    9, 10, 11, 12, 13, 14, 15];
+function researchMethod(){
+    console.log(numbers06.indexOf(10));
+    console.log(numbers06.indexOf(100));
+}
+
+function multipleOf13(element, index, array){
+    return (element %13 == 0);
+}
+
+function findMethod(){
+    console.log(numbers06.find(multipleOf13));
+    console.log(numbers06.findIndex(multipleOf13));
+}
+
+function includesMethod(){
+    console.log(numbers06.includes(15));
+    console.log(numbers06.includes(20));
+    let numbers07 = [7, 6, 5, 4, 3, 2, 1];
+    console.log(numbers07.includes(4,5));
+}
+
+function arrayToStringMethod(){
+    console.log(numbers06.toString());
+    const numbersString = numbers06.join("-");
+    console.log(numbersString);
+}
+
+let btResearch = document.getElementById("btResearch");
+let btFind = document.getElementById("btFind");
+let btInclude = document.getElementById("btIncludes");
+let btArrayToString = document.getElementById("btArrayToString");
+btResearch.addEventListener("click", researchMethod);
+btFind.addEventListener("click", findMethod);
+btInclude.addEventListener("click", includesMethod);
+btArrayToString.addEventListener("click", arrayToStringMethod);
