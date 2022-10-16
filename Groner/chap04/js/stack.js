73254e42ead0ee7075c25ff1ac62adc1e1251486
Walescko <walescko@gmail.com>
2022-10-16 19:29:54 -0300

    js - class stack

--- Groner/chap04/js/stack.js
+++ Groner/chap04/js/stack.js
@@ -1,25 +1,5 @@
-class Stack{
+class Stack {
     constructor() {
-        this.items=[];
-    }
-    //Methods
-    push(element) {
-        this.items.push(element);
-    }
-
-    pop(){
-        return this.items.pop();
-    }
-
-    peek(){
-        return this.items[this.items.length-1];
-    }
-
-    isEmpty(){
-        return this.items.length === 0;
-    }
-
-    size(){
-        return this.items.length;
+        this.items = [];
     }
 }
