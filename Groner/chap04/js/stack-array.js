c3de6f203bae156d1baeba9160bed4eb1c9f924a
Walescko <walescko@gmail.com>
2022-10-16 22:01:18 -0300

    js - stack class and methods of class

--- Groner/chap04/js/stack-array.js
+++ Groner/chap04/js/stack-array.js
@@ -27,3 +27,24 @@ class Stack{
         this.items=[];
     }
 }
+
+function stackUse(){
+    const stack = new Stack();
+    console.log(stack.isEmpty());
+    stack.push(5);
+    stack.push(8);
+    console.log(stack);
+    console.log(stack.peek());
+    console.log(stack.size());
+    stack.push(11);
+    console.log(stack.isEmpty());
+    stack.push(15);
+    console.log(stack.size());
+    stack.pop();
+    stack.pop();
+    console.log(stack.size());
+
+}
+
+let btStackUse = document.getElementById("btStackUse");
+btStackUse.addEventListener("click", stackUse);
--- Groner/chap04/js/stack.js
+++ Groner/chap04/js/stack.js
@@ -1,5 +1,57 @@
-class Stack {
+class Stack01{
     constructor() {
-        this.items = [];
+        this.count = 0;
+        this.items ={};
+    }
+
+    push(element){
+        this.items[this.count] = element;
+        this.count++;
+    }
+    size(){
+        return this.count;
+    }
+    isEmpty(){
+        return this.count === 0;
+    }
+    pop(){
+        if(this.isEmpty()){
+            return undefined;
+        }
+        this.count--;
+        const result = this.items[this.count];
+        delete this.items[this.count];
+        return result;
+    }
+
+    peek(){
+        if(this.isEmpty()){
+            return undefined;
+        }
+        return this.items[this.count-1];
+    }
+    clear(){
+        this.items = {};
+        this.count = 0;
+    }
+    toString(){
+        if(this.isEmpty()){
+            return '';
+        }
+        let objString = '${this.items[0]}';
+        for (let i = 1; i<= this.count; i++){
+            objString = '${objString},${this.items[i]}';
+        }
+        return objString;
     }
 }
+
+function stackCreate(){
+    const stack = new Stack01();
+    stack.push(5);
+    stack.push(9);
+    console.log(stack);
+}
+
+let btStackCreate = document.getElementById("btStackCreate");
+btStackCreate.addEventListener("click", stackCreate);
