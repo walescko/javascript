9863c7e556f5e29c4e9ffe2da401988670756918
Walescko <walescko@gmail.com>
2022-11-13 20:45:12 -0300

    js - remove element

--- Groner/chap06/js/DoublyNode.js
+++ Groner/chap06/js/DoublyNode.js
@@ -42,4 +42,29 @@ class DoublyLinkedList extends LinkedList{
             }
         return false;
         }
+        removeAt(index){
+            if (index >= 0 && index < this.count){
+                let current = this.head;
+                if (index === 0){
+                    this.head = current.next;
+                    if(this.count === 1){
+                        this.tail - undefined;
+                    } else {
+                        this.head.prev = undefined;
+                    }
+                } else if (index === this.count - 1){
+                    current = this.tail;
+                    this.tail = current.prev;
+                    this.tail.next = undefined;
+                } else {
+                    current - this.getElementAt(index);
+                    const previous = current.prev;
+                    previous.next = current.next;
+                    currente.next.prev = previous;
+                }
+                this.count--;
+                return current.element;
+            }
+            return undefined;
+        }
 }
