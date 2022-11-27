792e4d31b6e7ec501f2084d4d99157b9b0332d6f
Walescko <walescko@gmail.com>
2022-11-27 20:24:29 -0300

    js - Hash Table Separare 2

--- Groner/chap08/js/hashTableSeparateChainingRefactor.js
+++ Groner/chap08/js/hashTableSeparateChainingRefactor.js
@@ -1,90 +1,116 @@
-import {defaultToString} from "./util.js";
-import LinkedList from "../../chap06/js/linkedList.js";
 import {ValuePair} from "./valuePair.js";
+import {defaultToString} from "./util.js";
 
-class HashTableSeparateChaining{
+class HashTableSeparateChainingRefactor{
     constructor(toStrFn = defaultToString) {
         this.toStrFn = toStrFn;
         this.table = {};
     }
+
     //the same code of hashTable.js
-    loseloseHashCode(key){
-        if(typeof key === "number"){
+    loseloseHashCode(key) {
+        if (typeof key === "number") {
             return key;
         }
         const tableKey = this.toStrFn(key);
         let hash = 0;
-        for (let i = 0; i < tableKey.length; i++){
+        for (let i = 0; i < tableKey.length; i++) {
             hash += tableKey.charCodeAt(i);
         }
         return hash % 37;
     }
-    hashCode(key){
+
+    hashCode(key) {
         return this.loseloseHashCode(key);
     }
-    put(key, value){
-        if(key != null && value != null){
+
+    put(key, value) {
+        if (key != null && value != null) {
             const position = this.hashCode(key);
-            if (this.table[position] == null){
-                this.table[position] = new LinkedList();
+            if (this.table[position] == null) {
+                this.table[position] = new ValuePair(key, value);
+            } else {
+                let index = position + 1;
+                while (this.table[index] != null) {
+                    index++;
+                }
+                this.table[index] = new ValuePair(key, value);
             }
-            this.table[position].push(new ValuePair(key,value));
             return true;
         }
-        return false
+        return false;
     }
-    get(key){
+
+    get(key) {
         const position = this.hashCode(key);
-        const linkedList = this.table[position];
-        if (linkedList != null && !linkedList.isEmpty()){
-            let current = linkedList.getHead();
-            while (current != null){
-                if(current.element.key === key){
-                    return current.element.value;
-                }
-                current = current.next;
+        if (this.table[position] != null) {
+            if (this.table[position].key === key) {
+                retunr
+                this.table[position].value;
+            }
+            let index = position + 1;
+            while (this.table[index] != null && this.table[index].key !== key) {
+                index++;
+            }
+            if (this.table[index] != null && this.table[index].key === key) {
+                return this.table[position].value;
             }
         }
         return undefined;
     }
-    remove(key){
+
+    remove(key) {
         const position = this.hashCode(key);
-        const linkedList = this.table[position];
-        if(linkedList != null && !linkedList.isEmpty()) {
-            let current = linkedList.getHead();
-            while (current != null) {
-                if (current.element.key === key) {
-                    linkedList.remove(current.element);
-                    if (linkedList.isEmpty()) {
-                        delete this.table[position];
-                    }
-                    return true;
-                }
-                current = current.next;
+        if (this.table[position] != null) {
+            if (this.table[position].key === key) {
+                delete this.table[position];
+                this.verifyRemoveSideEffect(key, position);
+                return true;
             }
+            let index = position + 1;
+            while (this.table[index] != null && this.table[index].key !== key) {
+                index++;
+            }
+            if (this.table[index] != null && this.table[index].key === key) {
+                delete this.table[index];
+                this.verifyRemoveSideEffect(key, index);
+                return true;
+            }
+            return false;
         }
-        return false;
+    }
+    verifyRemoveSideEffect(key, removedPosition){
+        const hash = this.hashCode(key);
+        let index = removedPosition;
+        while (this.table[index] !=null){
+            const posHash = this.hashCode(this.table[index].key);
+                if(posHash <= hash || posHash <= removedPosition)
+                this.table[removedPosition] = this.table[index];
+                delete this.table[index];
+                removedPosition = index;
+            }
+            index++;
         }
 }
 
-function HashCollision() {
-    const hash = new HashTableSeparateChaining();
-    hash.put('Ygritte', 'ygritte@email.com');
-    hash.put('Jonathan', 'jonathan@email.com');
-    hash.put('Jamie', 'jamie@email.com');
-    hash.put('Jack', 'jack@email.com');
-    hash.put('Jasmine', 'jasmine@email.com');
-    hash.put('Jake', 'jake@email.com');
-    hash.put('Nathan', 'nathan@email.com');
-    hash.put('Athelstan', 'athelstan@email.com');
-    hash.put('Sue', 'sue@email.com');
-    hash.put('Aethelwulf', 'aethelwulf@email.com');
-    hash.put('Sargeras', 'sargeras@email.com');
+function HashCollisionRefactoring() {
+        const hash = new HashTableSeparateChainingRefactor();
+        hash.put('Ygritte', 'ygritte@email.com');
+        hash.put('Jonathan', 'jonathan@email.com');
+        hash.put('Jamie', 'jamie@email.com');
+        hash.put('Jack', 'jack@email.com');
+        hash.put('Jasmine', 'jasmine@email.com');
+        hash.put('Jake', 'jake@email.com');
+        hash.put('Nathan', 'nathan@email.com');
+        hash.put('Athelstan', 'athelstan@email.com');
+        hash.put('Sue', 'sue@email.com');
+        hash.put('Aethelwulf', 'aethelwulf@email.com');
+        hash.put('Sargeras', 'sargeras@email.com');
 
-    console.log(hash.hashCode("Sue"));
-    console.log(hash.hashCode("Aethelwulf"));
+        console.log(hash.hashCode("Sue"));
+        console.log(hash.hashCode("Aethelwulf"));
 
 }
 
-let btHashCollision = document.getElementById("btHashCollision");
-btHashCollision.addEventListener("click", HashCollision);
+let btHashCollisionRefactoring = document.getElementById("btHashCollisionRefactoring");
+btHashCollisionRefactoring.addEventListener("click", HashCollisionRefactoring);
