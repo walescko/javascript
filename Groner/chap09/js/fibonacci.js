e322f1ddb12e0707f9afff3540fb4a4c67e796ac
Walescko <walescko@gmail.com>
2022-11-27 21:59:50 -0300

    js - fibonacci n term of serie

---
+++ Groner/chap09/js/fibonacci.js
@@ -0,0 +1,14 @@
+function fibonacci(n){
+    if (n <= 1) return 0;
+    if (n <= 2) return 1;
+    return (fibonacci(n- 1) + fibonacci(n - 2));
+}
+
+function FibonacciC(){
+    let inNumber = document.getElementById("inNumber");
+    let n = Number(inNumber.value);
+    console.log("O " +n + "º termo da Série de Fibonacci é: " + fibonacci(n));
+}
+
+let btFibonacci = document.getElementById("btFibonacci");
+btFibonacci.addEventListener("click", FibonacciC);
