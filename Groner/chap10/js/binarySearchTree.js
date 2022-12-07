8297463bf0d65dfb09105b5872ff8df15756db54
Walescko <walescko@gmail.com>
2022-12-06 22:08:33 -0300

    js - binart search tree

--- Groner/chap10/js/binarySearchTree.js
+++ Groner/chap10/js/binarySearchTree.js
@@ -31,6 +31,23 @@ export default class BinarySearchTree{
         }
     }
 }
+function binaryTree() {
+    const tree = new BinarySearchTree();
+    tree.insert(11);
+    tree.insert(15);
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(9);
+    tree.insert(8);
+    tree.insert(10);
+    tree.insert(13);
+    tree.insert(12);
+    tree.insert(14);
+    tree.insert(20);
+    tree.insert(18);
+    tree.insert(25);
+    tree.insert(6);
+}
 
-const tree = new BinarySearchTree();
-tree.insert(11);
+let btBinaryTree = document.getElementById("btBinaryTree");
+btBinaryTree.addEventListener("click", binaryTree);
