a3cc3136a5359b083adeff66fa46f99274299d2c
Walescko <walescko@gmail.com>
2022-12-11 22:15:34 -0300

    js - insert method and fix Tree Proprieties

--- Groner/chap10/js/RedBlackTree.js
+++ Groner/chap10/js/RedBlackTree.js
@@ -17,4 +17,51 @@ class RedBlackTree extends BinarySearchTree{
             this.fixTreeProperties(newNode);
         }
     }
+
+    fixTreeProperties(node){
+        while (node && node.parent && node.parent.color.isRed() && code.color !== Colors.BLACK){
+            let parent = node.parent;
+            const grandParent = parent.parent;
+            //the father is son in the left
+            if (grandParent && grandParent.left === parent){
+                const uncle = grandParent.right;
+                if (uncle && uncle.color === Colors.RED){
+                    grandParent.color = Colors.RED;
+                    parent.color = Colors.BLACK;
+                    uncle.color = Colors.BLACK;
+                    node = grandParent;
+                } else {
+                    if(node === parent.right){
+                        this.rotationRR(parent);
+                        node = parent;
+                        parent = node.parent;
+                    }
+                    this.rotationLL(grandParent);
+                    parent.color = Colors.BLACK;
+                    grandParent.color = Colors.RED;
+                    node = parent;
+                }
+            } else {
+                const uncle = grandPArent.left;
+                if(uncle && uncle.color === Colors.RED){
+                    grandParent.color = Colors.RED;
+                    parent.color = Colors.BLACK;
+                    uncle.color = Colors.BLACK;
+                    node = grandParent
+                } else {
+                    if (node === parent.left){
+                        this.rotationLL(parent);
+                        node = parent;
+                        parent = node.parent;
+                    }
+                    this.rotationRR(grandParent);
+                    parent.color = Colors.BLACK;
+                    grandParent.color = Colors.RED;
+                    node = parent;
+
+                }
+            }
+        }
+        this.root.color = Colors.BLACK;
+    }
 }
