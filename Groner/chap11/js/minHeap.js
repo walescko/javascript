8f598305b7bea6eb1a3bf55de76a464d03f4152e
Walescko <walescko@gmail.com>
2022-12-13 23:52:24 -0300

    js - insert method and aux methods

--- Groner/chap11/js/minHeap.js
+++ Groner/chap11/js/minHeap.js
@@ -17,5 +17,28 @@ export class MinHeap{
         }
         return Math.floor((index-1)/2);
     }
-    
+
+    insert(value){
+        if(value != null){
+            this.heap.push(value);
+            this.siftUp(this.heap.length -1);
+            return true;
+        }
+        return false;
+    }
+    sifUp(index){
+        let parent = this.getParentIndex(index);
+        while (
+            index > 0 && this.compareFn(this.heap[parent]), this.heap[index] > Compare.BIGGER_THAN
+            ){
+            swap(this.heap, parent, index);
+            index = parent;
+            parent = this.getParentIndex(index);
+        }
+    }
+}
+function swap(array, a ,b){
+    const temp = array[a];
+    array[a] = array[b];
+    array[b] = temp;
 }
