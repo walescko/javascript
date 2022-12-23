e8334057d69318ae4f9d10352f0386494f45326d
Walescko <walescko@gmail.com>
2022-12-23 07:59:16 -0300

    js/html - Breath Firts Search alg!

--- Groner/chap12/js/breathFirstSearch.js
+++ Groner/chap12/js/breathFirstSearch.js
@@ -12,3 +12,26 @@ const initializedColor = vertices => {
     return color;
 };
 
+export const breathFirstSearch = (graph, starVertex, callback) => {
+    const vertices = graph.getVertices();
+    const adjList = graph.getAdjList();
+    const color = initializedColor(vertices);
+    const queue = new Queue();
+    queue.enqueue(starVertex);
+    while (!queue.isEmpty()){
+        const u = queue.dequeue();
+        const neigbors = adjList.get(u);
+        color[u] = Color.GREY;
+        for (let i = 0; i < neigbors.length; i++){
+            const w = neigbors[i];
+            if (color[w] === Colors.WHITE){
+                color[w] = Colors.GREY;
+                queue.enqueue(w);
+          }
+        }
+    color[u] = Colors.BLACK;
+        if (callback){
+            callback(u);
+        }
+    }
+};
