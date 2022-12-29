30895f751f4f1253546e9591c76a3919882b6339
Walescko <walescko@gmail.com>
2022-12-28 22:47:15 -0300

    js - buble sort

--- Groner/chap13/js/bubbleSort.js
+++ Groner/chap13/js/bubbleSort.js
@@ -11,3 +11,16 @@ function bubbleSort(array, compareFn = defaultCompare){
     }
     return array;
 }
+
+function createNonSortedArray(size){
+    const array = [];
+    for (let i = size; i > 0; i--){
+        array.push(i);
+    }
+    return array;
+}
+
+let array = createNonSortedArray(5);
+console.log(array.join());
+array = bubbleSort(array);
+console.log(array.join());
