b94acd89ca0eccbad53643cc2abfd7c28c2e8359
Walescko <walescko@gmail.com>
2022-12-29 06:29:14 -0300

    js - bubble sort best

--- Groner/chap13/js/bubbleSort.js
+++ Groner/chap13/js/bubbleSort.js
@@ -3,8 +3,8 @@ import {Compare, defaultCompare, swap} from "./util";
 function bubbleSort(array, compareFn = defaultCompare){
     const{length} = array;
     for (let i = 0; i < length; i++){
-        for (let j = 0; j < length; j++){
-            if (compareFn(arrat[j], array[j+1]) === Compare.BIGGER_THAN){
+        for (let j = 0; j < length-1; j++){
+            if (compareFn(array[j], array[j+1]) === Compare.BIGGER_THAN){
                 swap(array, j, j+1);
             }
         }
