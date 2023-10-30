bef33854af196ab44e20728282bc3793348add78
walescko <walescko@gmail.com>
2023-10-29 21:00:54 -0300

    ts - types

---
+++ typescript/LearningTS/chap02/types.ts
@@ -0,0 +1,13 @@
+// Inferred type: string
+let bestsong = Math.random() > 0.5 ? "Only Fools" : "My Life"
+
+let firstName = "Crystal"
+// firstName.length() //incorrect
+firstName.length //correct
+
+//let let wat //incorect sintaxe because let let in sequence - Error: "," expected.
+
+let lastName = "Levis"
+console.log(firstName + " " + lastName)
+lastName = "Lewis"
+console.log(firstName + " " + lastName)
