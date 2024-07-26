0fa80b8d6d8d8e14587203ffd9349a6ed4a6a653
walescko <walescko@gmail.com>
2024-07-26 04:27:17 -0300

    js/html - revenda

---
+++ EFI/cap02/js/revenda.js
@@ -0,0 +1,8 @@
+function mostrarPromocao(){
+    let inVeiculo = document.getElementById("inVeiculo");
+    let inPreco = document.getElementById("inPreco");
+    let outVeiculo = document.getElementById("outVeiculo");
+    let outEntrada = document.getElementById("outEntrada");
+    let outParcela = document.getElementById("outParcela");
+
+}
