3f8b27727944325245fdad2830ee09ff8d5e35f4
walescko <walescko@gmail.com>
2024-07-30 16:45:07 -0300

    peso ideal

--- EFI/cap03/js/pesoIdeal.js
+++ EFI/cap03/js/pesoIdeal.js
@@ -5,11 +5,34 @@ function calcularPeso (){
     let inAltura = document.getElementById("inAltura")
     let outResposta = document.getElementById("outResposta")
 
+    let nome = inNome.value 
+    let masculino = rbMasculino.checked 
+    let feminino = rbFeminino.checked
+    let altura = Number(inAltura.value)
+    let peso = 0
+
+    
     if (nome == "" || (masculino == false && feminino == false)) {
         alert("Por favor, informe o nome e selecione o sexo...")
         inNome.focus()
         return
     }
 
-    
+    if (altura == 0 || isNaN(altura)){
+        alert("Por favor, informe a altura corretamente...")
+        inAltura.focus();
+        return
+    }
+
+
+    if (masculino) {
+        peso = 22 * Math.pow(altura, 2);
+    } else {
+        peso = 21 * Math.pow(altura, 2)
+    }
+
+    outResposta.textContent = nome + ": seu peso ideal é " + peso.toFixed(3) + " kg."
 }
+
+let btResultado = document.getElementById("btCalcular")
+btResultado.addEventListener("click", calcularPeso)
