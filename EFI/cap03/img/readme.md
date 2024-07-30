3f078fae20bd9341e60be86b19156d8b6d63f413
walescko <walescko@gmail.com>
2024-07-30 15:38:18 -0300

    peso ideal

---
+++ EFI/cap03/img/readme.md
@@ -0,0 +1,11 @@
+As imagens são de autora de Carolina Kuwabata,
+da Editora Novatec. Elas fazem parte do material
+disponibilzado pela editora dos códigos fontes
+do livro.
+Lógica de Programação e Algoritmos com Javascript
+Autor: Edécio Fernando Iepsen
+Editora Novatec
+1ª Edição - 2018
+
+Quem quiser obter as imagens pode baixar direto no site da editora
+https://novatec.com.br/livros/logica-programacao-algoritmos-com-javascript/
---
+++ EFI/cap03/js/pesoIdeal.js
@@ -0,0 +1,15 @@
+function calcularPeso (){
+    let inNome = document.getElementById("inNome")
+    let rbMasculino = document.getElementById("rbMasculino")
+    let rbFeminino = document.getElementById("rbFeminino")
+    let inAltura = document.getElementById("inAltura")
+    let outResposta = document.getElementById("outResposta")
+
+    if (nome == "" || (masculino == false && feminino == false)) {
+        alert("Por favor, informe o nome e selecione o sexo...")
+        inNome.focus()
+        return
+    }
+
+    
+}
