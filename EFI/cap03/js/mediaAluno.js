0982b562593bcae7b691cf03b134dea564f67e48
Walescko Chimendes <walescko@WSC.intelbras.local>
2024-07-30 09:16:45 -0300

    js - average

---
+++ EFI/cap03/js/mediaAluno.js
@@ -0,0 +1,25 @@
+function calcularMedia(){
+    let inNome = document.getElementById("inNome")
+    let inNota01 = document.getElementById("inNota01")
+    let inNota02 = document.getElementById("inNota02")
+
+    let outSituacao = document.getElementById("outSituacao")
+    let outMedia = document.getElementById("outMedia")
+
+    let nome = inNome.value 
+    let nota01 = inNota01.value
+    let nota02 = inNota02.value
+
+    let media = (nota01 + nota02)/2
+
+    if (media >= 7) {
+        outSituacao.textContent = "Parabéns " + nome + "! Você foi aprovado(a)"
+        outSituacao.style.color = "blue"
+    } else {
+        outSituacao.textContent = "Ops " + nome + "... Você foi repovado"
+        outSituacao.style.color = "red"
+    }
+}
+
+let btResultado = document.getElementById("btResultado")
+btResultado.addEventListener("click", calcularMedia)
