60e816ffe0146b18a9b320eec6a84cd61f107920
walescko <walescko@gmail.com>
2024-08-05 11:01:16 -0300

    js - clear location ok

--- EFI/cap03/js/clear.js
+++ EFI/cap03/js/clear.js
@@ -1,13 +1,11 @@
 function limparCampos(){
     //uma primeira forma de limpar dados campo a campo
     
-    document.getElementById("inNome").value = ""
-    document.getElementById("rbMasculino").checked = false
-    document.getElementById("rbFeminino").checked = false
-    document.getElementById("inAltura").value = ""
-    document.getElementById("outResposta").textContent = ""
-
-    
+    // document.getElementById("inNome").value = ""
+    // document.getElementById("rbMasculino").checked = false
+    // document.getElementById("rbFeminino").checked = false
+    // document.getElementById("inAltura").value = ""
+    // document.getElementById("outResposta").textContent = "" 
 
     // uma segunda opção de limpar dados com releitura da página
     location.reload()
