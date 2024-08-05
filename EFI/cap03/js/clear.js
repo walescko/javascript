55982409e01eaf39608227ee872a30578fd47b0b
walescko <walescko@gmail.com>
2024-08-05 11:00:06 -0300

    js - clear reload full

--- EFI/cap03/js/clear.js
+++ EFI/cap03/js/clear.js
@@ -1,11 +1,20 @@
 function limparCampos(){
+    //uma primeira forma de limpar dados campo a campo
+    
     document.getElementById("inNome").value = ""
     document.getElementById("rbMasculino").checked = false
     document.getElementById("rbFeminino").checked = false
     document.getElementById("inAltura").value = ""
     document.getElementById("outResposta").textContent = ""
 
+    
+
+    // uma segunda opção de limpar dados com releitura da página
+    location.reload()
+
+    // devolvendo a página no ponto desejado independe da forma
     document.getElementById("inNome").focus()
+
 }
 
 let btLimpar = document.getElementById("btLimpar")
